a7855ee7f460412e9146d15c7d6fa6170975fbec
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-12-12 21:31:26 +0100

    Lägger till UserRepository och UserService

---
+++ StefansSuperShop/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StefansSuperShop.Repositories
+{
+    public interface IUserRepository
+    {
+        public IdentityUser GetUser(string email);
+        public void RegisterUser(IdentityUser user, string password = null, string role = null);
+        public void DeleteUser(IdentityUser user);
+        public void UpdateUser(IdentityUser user);
+
+    }
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserRepository(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IdentityUser GetUser(string email)
+        {
+            return _userManager.FindByEmailAsync(email).Result;
+        }
+
+        public void RegisterUser(IdentityUser user, string password, string role)
+        {
+            if (password != null)
+            {
+                _userManager.CreateAsync(user, password);
+                _userManager.AddToRoleAsync(user, role);
+            }
+            else
+            {
+                _userManager.CreateAsync(user);
+            }
+        }
+
+        public void DeleteUser(IdentityUser user)
+        {
+            _userManager.DeleteAsync(user);
+        }
+
+        public void UpdateUser(IdentityUser user)
+        {
+            _userManager.UpdateAsync(user);
+        }
+    }
+}
---
+++ StefansSuperShop/Services/UserService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StefansSuperShop.Repositories;
+
+namespace StefansSuperShop.Services
+{
+    interface IUserService
+    {
+        public void RegisterUser(string email, string password);
+        public void RegisterNewsletterOnly(string email);
+        public void RegisterUpgradeFromNewsletter(string email, string password);
+        public void DeleteUser(string email);
+    }
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void RegisterUser(string email, string password)
+        {
+            if (!CheckEmail(email))
+            {
+                throw new System.Exception("Email is already in use");
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = email,
+                Email = email,
+                EmailConfirmed = true
+            };
+
+            var role = "Customer";
+
+            _userRepository.RegisterUser(user, password, role);
+        }
+
+        public void RegisterNewsletterOnly(string email)
+        {
+            if (!CheckEmail(email))
+            {
+                throw new System.Exception("Email is already in use");
+            }
+
+            var user = new IdentityUser
+            {
+                Email = email,
+                EmailConfirmed = true
+                // lägg till NewsletterActive = true när vi mergat in från andra feature branchen
+            };
+
+            _userRepository.RegisterUser(user);
+        }
+
+        public void RegisterUpgradeFromNewsletter(string email, string password)
+        {
+
+        }
+
+        public void DeleteUser(string email)
+        {
+            var user = _userRepository.GetUser(email);
+
+            if (user != null)
+            {
+                _userRepository.DeleteUser(user);
+            }
+        }
+
+        private bool CheckEmail(string email)
+        {
+            return _userRepository.GetUser(email) != null;
+        }
+    }
+}
