936012ded58619462e3088998a54b35a51aaa847
Niklas Negri <69984406+NiklasNegri@users.noreply.github.com>
2022-12-13 11:09:09 +0100

    Skapar logik för Newsletter repository och service

--- StefansSuperShop/Repositories/NewsletterRepository.cs
+++ StefansSuperShop/Repositories/NewsletterRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace StefansSuperShop.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using StefansSuperShop.Data.Entities;
+using StefansSuperShop.Data;
+using System.Collections.Generic;
+
+namespace StefansSuperShop.Repositories
 {
-    public class NewsletterRepository
+    public interface INewsletterRepository
     {
+        public Newsletter GetById(int id);
+        public IEnumerable<Newsletter> GetAll();
+        public void CreateNewsletter(Newsletter newsletter);
+        public void EditNewsletter(Newsletter newsletter);
+        public void DeleteNewsletter(Newsletter newsletter);
+    }
+
+    public class NewsletterRepository : INewsletterRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsletterRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Newsletter GetById(int id)
+        {
+           return _context.Newsletters.Find(id);
+        }
+
+        public IEnumerable<Newsletter> GetAll()
+        {
+            return _context.Newsletters;
+        }
+
+        public void CreateNewsletter(Newsletter newsletter)
+        {
+            _context.Newsletters.Add(newsletter);
+            _context.SaveChanges();
+        }
+
+        public void EditNewsletter(Newsletter newsletter)
+        {
+            _context.Newsletters.Update(newsletter);
+            _context.SaveChanges();
+        }
+
+        public void DeleteNewsletter(Newsletter newsletter)
+        {
+            _context.Newsletters.Remove(newsletter);
+            _context.SaveChanges();
+        }
     }
 }
--- StefansSuperShop/Services/NewsletterService.cs
+++ StefansSuperShop/Services/NewsletterService.cs
@@ -1,6 +1,50 @@
-﻿namespace StefansSuperShop.Services
+﻿using StefansSuperShop.Data.Entities;
+using StefansSuperShop.Repositories;
+
+namespace StefansSuperShop.Services
 {
     public class NewsletterService
     {
+        private readonly INewsletterRepository _newsletterRepository;
+
+        public NewsletterService(INewsletterRepository newsletterRepository)
+        {
+            _newsletterRepository = newsletterRepository;
+        }
+
+        public void CreateNewsletter(string title, string content)
+        {
+            var newsletter = new Newsletter { Title = title, Content = content };
+            _newsletterRepository.CreateNewsletter(newsletter);
+        }
+
+        public void EditNewsletter(int id, string title, string content)
+        {
+            var newsletter = CheckNewsletterExists(id);
+
+            if (title != null) newsletter.Title = title;
+            if (content != null) newsletter.Content = content;
+
+            _newsletterRepository.EditNewsletter(newsletter);
+        }
+
+        public void DeleteNewsletter(int id)
+        {
+            var newsletter = CheckNewsletterExists(id);
+
+            _newsletterRepository.DeleteNewsletter(newsletter);
+        }
+
+        private Newsletter CheckNewsletterExists(int id)
+        {
+            var newsletter = _newsletterRepository.GetById(id);
+
+            if (newsletter == null)
+            {
+                throw new System.Exception("Newsletter with that ID does not exist");
+            }
+
+            return newsletter;
+        }
     }
 }
