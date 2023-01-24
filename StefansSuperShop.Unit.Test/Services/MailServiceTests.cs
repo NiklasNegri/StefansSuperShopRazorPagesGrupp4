b3fb808d4e561edfc930ceaaba372c611328b2e7
Niklas Negri <69984406+NiklasNegri@users.noreply.github.com>
2023-01-24 11:28:54 +0100

    finished writing tests for mailservice

--- StefansSuperShop.Unit.Test/Services/MailServiceTests.cs
+++ StefansSuperShop.Unit.Test/Services/MailServiceTests.cs
@@ -8,33 +8,57 @@ using StefansSuperShop.Services;
 using StefansSuperShop.Data.Model;
 using Microsoft.Extensions.Options;
 using StefansSuperShop.Configuration;
+using AutoFixture;
 
 namespace StefansSuperShop.Unit.Test.Services
 {
     public class MailServiceTests
     {
         private readonly Mock<IOptions<MailSettings>> _settingsMock;
+        private Fixture _fixture = new ();
         private readonly MailService _sut;
 
         public MailServiceTests()
         {
             _settingsMock = new Mock<IOptions<MailSettings>>();
             _sut = new MailService(_settingsMock.Object);
+
+            _settingsMock.Setup(x => x.Value.DisplayName).Returns("Display Name");
+            _settingsMock.Setup(x => x.Value.From).Returns("test@test.se");
+            _settingsMock.Setup(x => x.Value.Host).Returns("host.host");
+            _settingsMock.Setup(x => x.Value.Port).Returns(000);
+            _settingsMock.Setup(x => x.Value.UserName).Returns("test@username.se");
+            _settingsMock.Setup(x => x.Value.Password).Returns("Abc123!");
+            _settingsMock.Setup(x => x.Value.UseSSL).Returns(false);
+            _settingsMock.Setup(x => x.Value.UseStartTls).Returns(true);
         }
 
         [Fact]
-        public async void SendAsync_ValidInput_SendMail()
+        public async void SendAsync_ValidInput_ThrowsNoErrors()
         {
-            
+            MailData mailData = _fixture.Create<MailData>();
+
+            await _sut.SendAsync(mailData, new CancellationToken());
         }
 
         [Fact]
-        public async void SendAsync_InvalidInput_ShowError()
+        public async void SendAsync_NoSubject_ThrowsErrors()
         {
-            var mailData = new MailData()
-            {
+            MailData mailData = _fixture.Create<MailData>();
 
-            };
+            mailData.Subject = null;
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _sut.SendAsync(mailData, new CancellationToken()));
+        }
+
+        [Fact]
+        public async void SendAsync_NoRecipients_ThrowsErrors()
+        {
+            MailData mailData = _fixture.Create<MailData>();
+
+            mailData.To = null;
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _sut.SendAsync(mailData, new CancellationToken()));
         }
     }
 }
