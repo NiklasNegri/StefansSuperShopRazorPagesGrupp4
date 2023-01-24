381cb18ad50af3fe4a00829bb73d6cfd505b820c
Niklas Negri <69984406+NiklasNegri@users.noreply.github.com>
2023-01-24 10:24:39 +0100

    add mailservice test

---
+++ StefansSuperShop.Unit.Test/Services/MailServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StefansSuperShop.Services;
+using StefansSuperShop.Data.Model;
+using Microsoft.Extensions.Options;
+using StefansSuperShop.Configuration;
+
+namespace StefansSuperShop.Unit.Test.Services
+{
+    public class MailServiceTests
+    {
+        private readonly Mock<IOptions<MailSettings>> _settingsMock;
+        private readonly MailService _sut;
+
+        public MailServiceTests()
+        {
+            _settingsMock = new Mock<IOptions<MailSettings>>();
+            _sut = new MailService(_settingsMock.Object);
+        }
+
+        [Fact]
+        public async void SendAsync_ValidInput_SendMail()
+        {
+            
+        }
+
+        [Fact]
+        public async void SendAsync_InvalidInput_ShowError()
+        {
+            var mailData = new MailData()
+            {
+
+            };
+        }
+    }
+}
