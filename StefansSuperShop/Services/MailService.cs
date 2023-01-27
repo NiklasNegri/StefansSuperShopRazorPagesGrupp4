74d7a95b00994608b7dd81101ef3042102708f06
Fredrik Lam <fredrik.didi.lam@gmail.com>
2023-01-27 19:33:56 +0100

    contact-mail-template

--- StefansSuperShop/Services/MailService.cs
+++ StefansSuperShop/Services/MailService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -57,8 +58,8 @@ public class MailService : IMailService
 
             // Add Content to Mime Message
             var body = new BodyBuilder();
-            mail.Subject = mailData.Subject;
-            body.HtmlBody = mailData.Body;
+            mail.Subject = mailData.Subject.Trim().ToUpper();
+            body.HtmlBody = GetMailTemplate(mailData.Body).Trim();
             mail.Body = body.ToMessageBody();
 
             #endregion
@@ -92,5 +93,17 @@ public class MailService : IMailService
     {
         foreach (var mailAddress in mailData.Where(x => !string.IsNullOrWhiteSpace(x)))
             mailList.Add(MailboxAddress.Parse(mailAddress.Trim()));
+    }
+
+    private static string GetMailTemplate(string body)
+    {
+        string filePath1 = "Data/Mail/Templates/index.html";
+        StreamReader str = new StreamReader(filePath1);
+        string mailTemplateText = str.ReadToEnd();
+        str.Close();
+
+        mailTemplateText = mailTemplateText.Replace("[This text till be replaced with input from user]", body);
+
+        return mailTemplateText;
     }
 }
